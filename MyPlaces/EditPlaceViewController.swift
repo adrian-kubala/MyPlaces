ee9d2e6421e973bd5aa94b0ca83806668b5e12e0
Adrian Kubała <adrian.kubala@gmail.com>
2017-04-15 20:18:50 +0200

    Implement adding picture

--- MyPlaces/EditPlaceViewController.swift
+++ MyPlaces/EditPlaceViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class EditPlaceViewController: UIViewController {
+class EditPlaceViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
   
   @IBOutlet weak var placeImageView: UIImageView!
   @IBOutlet weak var placeNameTextField: UITextField!
+  @IBOutlet weak var photoSourceControl: UISegmentedControl!
+  @IBOutlet weak var addPhotoButton: UIButton!
+  @IBOutlet weak var applyButton: TopSlicedButton!
   
   weak var delegate: EditPlaceViewControllerDelegate?
   
@@ -22,6 +25,53 @@ class EditPlaceViewController: UIViewController {
     
     placeNameTextField.text = place.name
     placeImageView.image = place.photo
+    setupSubviews()
+  }
+  
+  private func setupSubviews() {
+    let cornerRadius: CGFloat = 5
+    
+    placeImageView.layer.cornerRadius = cornerRadius
+    photoSourceControl.layer.cornerRadius = cornerRadius
+    addPhotoButton.layer.cornerRadius = addPhotoButton.bounds.size.width / 2
+  }
+  
+  @IBAction func addPicture(_ sender: Any) {
+    
+    let cameraIsAvailable = UIImagePickerController.isSourceTypeAvailable(.camera)
+    let photoLibraryIsAvailable = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+    
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    
+    if photoSourceControl.selectedSegmentIndex == 0 && cameraIsAvailable {
+      imagePicker.sourceType = .camera
+      imagePicker.allowsEditing = false
+      present(imagePicker, animated: true, completion: nil)
+    } else if photoLibraryIsAvailable {
+      imagePicker.sourceType = .photoLibrary
+      imagePicker.allowsEditing = true
+      present(imagePicker, animated: true, completion: nil)
+    }
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    let cropRect = info["UIImagePickerControllerCropRect"] as? CGRect
+    var originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage
+    
+    if let croppedRect = cropRect {
+      originalImage = originalImage?.cropToRect(croppedRect)
+    } else {
+      originalImage = originalImage?.cropToBounds(width: placeImageView.bounds.size.width, height: placeImageView.bounds.size.height)
+    }
+    
+    placeImageView.image = originalImage
+    dismiss(animated: true, completion: nil)
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
   }
   
   @IBAction func applyChanges() {
