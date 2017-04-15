ddc604ed291d03a73987bb9f02286448ddb2d42e
Adrian Kubała <adrian.kubala@gmail.com>
2017-04-15 20:04:47 +0200

    Implement applyChanges method

--- MyPlaces/EditPlaceViewController.swift
+++ MyPlaces/EditPlaceViewController.swift
@@ -24,4 +24,24 @@ class EditPlaceViewController: UIViewController {
     placeImageView.image = place.photo
   }
   
+  @IBAction func applyChanges() {
+    guard let isEmpty = placeNameTextField.text?.isEmpty, isEmpty == false else {
+      let alertController = UIAlertController(title: "Did not provide a new title", message: "You need to enter a title to add a new place.", preferredStyle: .alert)
+      let alertAction = UIAlertAction(title: "OK", style: .default) { action in
+        self.placeNameTextField.becomeFirstResponder()
+      }
+      alertController.addAction(alertAction)
+      alertController.view.tintColor = UIColor.black
+      present(alertController, animated: true, completion: nil)
+      
+      return
+    }
+    
+    place.name = placeNameTextField.text!
+    place.photo = placeImageView.image!
+    
+    delegate?.didEditPlace()
+    _ = navigationController?.popViewController(animated: true)
+  }
+  
 }
